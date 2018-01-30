316fc3462a41350f08edfa67e9d30cef98bb673e
Aas Suhendar <aas.suhendar@gmail.com>
2018-01-31 06:27:58 +0700

    Initial unit test using jest

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
@@ -34,26 +33,19 @@ if (process.env.NODE_ENV === 'test') {
   app.use(logger('dev'))
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/api/todo-list', todos)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+  res.status(404).send({
+    status: false,
+    code: 'NOT-FOUND',
+    message: 'Endpoint notfound'
+  })
 })
 
 // error handler
