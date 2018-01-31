658845f0ce37fca9908e47ed33443c5d2352f123
Aas Suhendar <aas.suhendar@gmail.com>
2018-01-31 17:11:30 +0700

    add scenario cucumber

--- features/support/TodoSteps.js
+++ features/support/TodoSteps.js
@@ -3,20 +3,22 @@ const { Given, When, Then } = require('cucumber')
 const { expect } = require('chai')
 
 Given('Melani memiliki pekerjaan yang harus dimasukan ke sistem dengan judul {string}',
-  function (title, callback) {
+  function (title) {
     // Write code here that turns the phrase above into concrete actions
 
     // callback(null, 'pending')
-    expect('Menyiapkan ruang rapat').to.eql(title)
+    // expect('Menyiapkan ruang rapat').to.eql(title)
+    expect('Menyiapkan ruang rapat').to.equal(title)
   })
 
-When('Melani memasukan pekerjaan', function (newTodo, callback) {
+When('Melani memasukan pekerjaan', function (newTodo) {
   // Write code here that turns the phrase above into concrete actions
   //   callback(null, 'pending')
   // return true
 })
 
-Then('Sistem menyimpan pekerjaan tersebut dan menampilkan pesan {string}', function (string, callback) {
+Then('Sistem menyimpan pekerjaan tersebut dan menampilkan pesan {string}', function (message) {
   // Write code here that turns the phrase above into concrete actions
   //   callback(null, 'pending')
+  expect('Sukses Menyimpan Pekerjaan').to.equal(message)
 })
