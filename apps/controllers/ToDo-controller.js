1dce7ba4ce0603f3d81faab434c7bd6dea8d5769
Aas Suhendar <aas.suhendar@gmail.com>
2018-01-31 07:46:26 +0700

    Add API Update and Delete Todos

--- apps/controllers/ToDo-controller.js
+++ apps/controllers/ToDo-controller.js
@@ -10,11 +10,30 @@ module.exports = {
           message: 'Get list todo failed',
           error: err
         })
-      }else {
+      } else {
         res.status(200).json({
           status: true,
           code: 'GET-LIST-TODO',
           message: 'Get list todo successfuly',
+          data: todos
+        })
+      }
+    })
+  },
+  getTodosById: (req, res) => {
+    ToDo.findById(req.params.id, (err, todos) => {
+      if (err) {
+        res.status(500).json({
+          status: false,
+          code: 'GET-TODO',
+          message: 'Get todo failed',
+          error: err
+        })
+      } else {
+        res.status(200).json({
+          status: true,
+          code: 'GET-TODO',
+          message: 'Get todo successfuly',
           data: todos
         })
       }
@@ -35,11 +54,55 @@ module.exports = {
           message: 'Insert new todo failed',
           error: err
         })
-      }else {
+      } else {
         res.status(200).json({
           status: true,
           code: 'INSERT-TODO',
           message: 'Insert new todo successfuly',
+          data: todo
+        })
+      }
+    })
+  },
+  updateTodo: (req, res) => {
+    ToDo.findByIdAndUpdate(req.body._id, {
+      $set: {
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status
+      }
+    }, {new: true}, (err, todo) => {
+      if (err) {
+        res.status(500).json({
+          status: false,
+          code: 'UPDATE-TODO',
+          message: 'Update new todo failed',
+          error: err
+        })
+      } else {
+        res.status(200).json({
+          status: true,
+          code: 'UPDATE-TODO',
+          message: 'Update new todo successfuly',
+          data: todo
+        })
+      }
+    })
+  },
+  deleteTodo: (req, res) => {
+    ToDo.remove({_id: req.body._id}, (err, todo) => {
+      if (err) {
+        res.status(500).json({
+          status: false,
+          code: 'DELETE-TODO',
+          message: 'Delete new todo failed',
+          error: err
+        })
+      } else {
+        res.status(200).json({
+          status: true,
+          code: 'DELETE-TODO',
+          message: 'Delete new todo successfuly',
           data: todo
         })
       }
--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,23 @@ var router = express.Router()
 var ToDoList = require('../apps/controllers/ToDo-controller')
 
 router.get('/', function (req, res) {
-  ToDoList.getAllTodos(req,res)
+  ToDoList.getAllTodos(req, res)
+})
+
+router.get('/:id', function (req, res) {
+  ToDoList.getTodosById(req, res)
 })
 
 router.post('/', function (req, res) {
-  ToDoList.insertTodo(req,res)
+  ToDoList.insertTodo(req, res)
+})
+
+router.put('/', function (req, res) {
+  ToDoList.updateTodo(req, res)
+})
+
+router.delete('/', function (req, res) {
+  ToDoList.deleteTodo(req, res)
 })
 
 module.exports = router
