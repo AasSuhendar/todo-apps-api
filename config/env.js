50859e074c2b58554feb6dba545c7eaf26a3c5d1
Aas Suhendar <aas.suhendar@gmail.com>
2019-12-16 16:22:23 +0700

    update sonar

--- config/env.js
+++ config/env.js
@@ -1,6 +1,5 @@
 module.exports = {
   database_dev: process.env.MONGO_URL || 'mongodb://127.0.0.1/todo-list',
-  // database_test: process.env.MONGO_URL || 'mongodb://127.0.0.1/todo-list-test',
   // db test to mongodb in openshift
   database_test: process.env.MONGO_URL || 'mongodb://127.0.0.1/todo-list'
 }
