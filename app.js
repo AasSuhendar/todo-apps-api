dae86e5683f933fa088cd3c3d30eda4a70a5bf69
Aas Suhendar <aas.suhendar@gmail.com>
2019-12-16 16:02:19 +0700

    update path url api

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'test') {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', index)
-app.use('/api/todo-list', todos)
+app.use('/api/todo', todos)
 
 // catch 404 and forward to error handler
 app.use(function (req, res) {
